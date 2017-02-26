2687b49e7821e8cc506dd9ad5a9c508a164b37de
ayapapa <ayapapajapan@yahoo.co.jp>
2017-02-26 15:20:02 +0900

    modified scripts for #16

--- mysql/start.sh
+++ mysql/start.sh
@@ -8,19 +8,26 @@ service mysql start
 OK=-1
 while [ "${OK}" != "0" ]; do
   echo connecting to db
-  mysql -e "SELECT user FROM user" mysql
+  mysql -e "SELECT user FROM user" mysql > /dev/null
   OK=$?
   sleep 5
 done
 
 echo "success to connect to mysql db"
 
+# create alminium db
+DB_EXIST=$(mysql -e "SHOW DATABASES" mysql | grep alminium)
+if [ "${DB_EXIST}" = "" ]; then
+  mysql -e "CREATE DATABASE alminium DEFAULT CHARACTER SET utf8"
+fi
+
 # add remote alminium user
-MYSQL_USERS=$(mysql -e "SELECT user, host FROM user" mysql)
-if [ "$(echo ${MYSQL_USERS} | grep alminium | grep %)" = "" ]; then
+REMOTE_USER_EXIST=$(mysql -e "SELECT user,host FROM user" mysql | grep alminium | grep %)
+if [ "${REMOTE_USER_EXIST}" = "" ]; then
   mysql -e "GRANT ALL PRIVILEGES ON alminium.* TO 'alminium'@'%' IDENTIFIED BY 'alminium'"
 fi
 
 # keep running this docker
 sleep infinity
+
 
